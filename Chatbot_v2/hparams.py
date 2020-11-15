e8262e9e7abac4aa32e4141290d63bb69893adbe
choi-hyunkyu <69423543+choi-hyunkyu@users.noreply.github.com>
2020-11-15 13:37:27 +0900

    Update hparams.py

--- Chatbot_v2/hparams.py
+++ Chatbot_v2/hparams.py
@@ -1,22 +1,34 @@
 '''
 Data Preprocessing part
 '''
-corpus_name = "normal chatbot data"
+
+# PATH
+import os
+
 PATH = './data/normal chatbot data/'
+corpus_name = "normal chatbot data"
 before_filename = 'speech_data.json'
 after_filename = 'speech_data.txt'
+
+
+corpus = os.path.join("data", corpus_name)
+datafile = os.path.join(corpus, after_filename)
+
+save_dir = os.path.join("data", "save")
+
+# preprocessing setting
 MAX_LENGTH = 20  # 고려할 문장의 최대 길이
 MIN_COUNT = 3    # 제외할 단어의 기준이 되는 등장 횟수
 
 '''
 Network part
 '''
-# 기본 단어 토큰 값
+# token value
 PAD_token = 0  # 짧은 문장을 채울(패딩, PADding) 때 사용할 제로 토큰
 SOS_token = 1  # 문장의 시작(SOS, Start Of Sentence)을 나타내는 토큰
 EOS_token = 2  # 문장의 끝(EOS, End Of Sentence)을 나태는 토큰
 
-# 모델을 설정합니다
+# model setting
 model_name = 'cb_model'
 attn_model = 'dot'
 #attn_model = 'general'
@@ -33,7 +45,7 @@ batch_size = 64
 loadFilename = None
 checkpoint_iter = 5000
 
-# 학습 및 최적화 설정
+# train setting
 clip = 50.0
 teacher_forcing_ratio = 1.0
 learning_rate = 0.0001
